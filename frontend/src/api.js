88e44cc30347696bd57e29c583483753ae004671
Maryyy-ux <maria.fernandez703@yahoo.com>
2025-02-23 11:19:58 +0100

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,6 @@
 // src/api.jsx
-const BASE_URL = "https://api.jikan.moe/v4";
+const BASE_URL = "https://api.jikan.moe/v4"; // API Jikan
+const API_URL = import.meta.env.VITE_API_URL; // URL backend on Render
 
 export const searchAnime = async (query) => {
     const response = await fetch(`${BASE_URL}/anime?q=${encodeURIComponent(query)}&sfw`);
@@ -15,6 +16,13 @@ export const fetchAnimeDetails = async (id) => {
 
 export const fetchMangaDetails = async (id) => {
     const response = await fetch(`${BASE_URL}/manga/${id}`);
+    if (!response.ok) throw new Error(`Error: ${response.status}`);
+    return response.json();
+};
+
+//  Backend on Render
+export const fetchFromBackend = async (endpoint) => {
+    const response = await fetch(`${API_URL}/${endpoint}`);
     if (!response.ok) throw new Error(`Error: ${response.status}`);
     return response.json();
 };
