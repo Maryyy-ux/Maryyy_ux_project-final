54480cb35fadb3ebdd87307ae7f14b1dd75cef78
Maryyy-ux <maria.fernandez703@yahoo.com>
2025-02-23 21:09:22 +0100

    Update CookiesPolicy.jsx

--- src/pages/CookiesPolicy.jsx
+++ src/pages/CookiesPolicy.jsx
@@ -4,7 +4,27 @@ const CookiesPolicy = () => {
     return (
         <div className="legal-page">
             <h1>Cookies policy</h1>
-            <p>Aquí irá el contenido de la política de cookies...</p>
+            <p>Effective Date: 09 02 2025</p>
+
+            <p>This Cookie Policy explains how Akuma Shirizu uses cookies and similar technologies to enhance your experience when you visit our website.</p>
+
+            <p>1. What Are Cookies? Cookies are small text files that are stored on your device when you visit a website. They allow the website to recognize your device and remember your preferences.</p>
+
+            <p>2. Types of Cookies We Use We use the following types of cookies on our website:</p>
+
+            <p>Essential Cookies: These cookies are necessary for the operation of the website and enable basic features such as page navigation.</p>
+
+            <p>Analytical Cookies: These cookies collect information about how visitors use the website, such as which pages are visited most often. This helps us improve the website’s performance.</p>
+
+            <p>Functional Cookies: These cookies remember your preferences and choices to improve your experience on the website.</p>
+
+            <p>3. Third-Party Cookies. We may also use third-party services, such as Google Analytics, which place cookies on your device to collect information on our behalf. These third parties may use this information to analyze trends, administer the website, and improve services.</p>
+
+            <p>4. Managing Cookies. You can control cookie settings in your browser settings. You can choose to block or delete cookies, but please be aware that some features of the website may not work properly if you disable them.</p>
+
+            <p>5. Consent. By continuing to use our website, you consent to the use of cookies as described in this Cookie Policy.</p>
+
+            <p>6. Changes to Cookie Policy We reserve the right to update this Cookie Policy at any time. Any changes will be posted on this page, and the effective date will be updated accordingly.</p>
         </div>
     );
 };
